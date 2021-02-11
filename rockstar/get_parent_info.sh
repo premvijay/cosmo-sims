5c1ef0bc496a0819e874ad7dda05cd3edb523209
cprem <premv@iucaa.in>
2021-02-11 16:38:40 +0530

    generalised to other box sizes

--- rockstar/get_parent_info.sh
+++ rockstar/get_parent_info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export simnm=${simnm:-L200_N256_Cp18} rund=${rund:-r1}
+export simnm=${simnm:-L200_N256_Cp18} rund=${rund:-r1} L=${L:-200}
 export dir_sim_data=/scratch/cprem/sims/$simnm/$rund/
 cd ${dir_sim_data}/halos_rs
-parallel -j 26 "find_parents out_{}.list 200 > out_wp_{}.list" ::: {0..50}
+parallel -j 26 "find_parents out_{}.list $L > out_wp_{}.list" ::: {0..50}
 # find_parents out
