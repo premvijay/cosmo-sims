c182f674d808a81f6f7e5f0aeb64b7ae5d01806f
cprem <premv@iucaa.in>
2021-03-14 01:51:37 +0530

    for gadget binary 201 snaps

---
+++ rockstar/gen-param_gad_bin.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo $2
+eval "$2"
+
+dir_sim_data=/scratch/cprem/sims/$simnm/$rund/
+dir_snap=$dir_sim_data/snaps/
+
+mkdir -p ${dir_sim_data}/halos_rs/
+# mkdir -p $1
+
+printf '%s\n' "INBASE = $dir_snap
+FORCE_RES = 0.0065
+NUM_SNAPS = 201
+STARTING_SNAP = 0
+
+NUM_WRITERS = 32
+FORK_PROCESSORS_PER_MACHINE = 32
+
+OUTBASE = ${dir_sim_data}/halos_rs/
+FILENAME = snapshot_<snap>.<block>
+NUM_BLOCKS = 8
+
+FILE_FORMAT = GADGET
+GADGET_LENGTH_CONVERSION = 1
+GADGET_MASS_CONVERSION = 1e+10
+
+PARALLEL_IO = 1
+FORK_READERS_FROM_WRITERS = 1
+PERIODIC = 1
+
+TEMPORAL_HALO_FINDING = 1
+STRICT_SO_MASSES = 0
+
+MIN_HALO_OUTPUT_SIZE = 30
+MIN_HALO_PARTICLES = 20
+UNBOUND_THRESHOLD = 0.5
+
+OVERLAP_LENGTH = 3" > $1
