e10c9c8df1e5e0fbe43ed6bd9532ec6a2700c6e2
cprem <premv@iucaa.in>
2021-03-27 18:21:21 +0530

    update this based on last two commit

--- scripts-common/bash/create-siminfo.sh
+++ scripts-common/bash/create-siminfo.sh
@@ -8,13 +8,14 @@ boxsize=${1:-200}
 Npart=${2:-512}
 cosmology=${3:-p18}
 bary=${bary:-no}
+ngenic=${ngenic:-no}
 
-export simnm="L${boxsize}_N${Npart}_C${cosmology}" rund=${rund:-r1}
+export simnm="L${boxsize}_N${Npart}_C${cosmology}" rund=${rund:-r1} seed=${seed-8899}
 if [ "$bary" = yes ]; then export simnm=${simnm}_bar; fi
 
 export softlen1=${softlen1:-0.0065} tstep=${tstep:-0.01}
 
-z_in=${z_in:-24}
+export z_in=${z_in:-24}
 
 echo Using values: $boxsize, $Npart, $cosmology, $rund, $z_in, $simn
 
@@ -24,13 +25,17 @@ dir_root=$HOME/cosmo-sims/
 
 dir_camb=$dir_root/camb/
 dir_mono=$dir_root/monofonic/
+dir_music=$dir_root/music/
 dir_gad=$dir_root/gadget4/
 dir_vel=$dir_root/velociraptor/
 
 mkdir -p $dir_mono/$simnm/
-mkdir -p $dir_gad/$simnm/
+mkdir -p $dir_music/$simnm/
+mkdir -p $dir_gad/$simnm/$rund
 
-echo "grid=$((2*Npart));nmpil=2;ncpus=32" > $dir_gad/$simnm/compile.info
+
+echo "grid=$((2*Npart));nmpil=2;ncpus=32;bary=$bary;ngenic=$ngenic" > $dir_gad/$simnm/$rund/compile.info
+
 case $cosmology in
   p18)
     Om0=0.3063375;OmL=0.6936625;Omb=0.0484103;h_par=0.6781;ns=0.9677;sig8=0.815
@@ -46,11 +51,12 @@ case $cosmology in
 esac
 
 echo "$Om0, $OmL, $Omb, $h_par, $ns, $sig8" > $dir_camb/${cosmology}.info   # To compile gadget
-#To generate parameter file for monofonIC
-echo "Om0=$Om0;OmL=$OmL;Omb=$Omb;h_par=$h_par;boxsize=$boxsize;Npart=$Npart;z_in=$z_in;cosmo=$cosmology;softlen1=$softlen1;timestep=$tstep;bary=$bary" > $dir_gad/$simnm/param_$rund.info
 #To generate parameter file for GADGET-4
-echo "Om0=$Om0;OmL=$OmL;Omb=$Omb;h_par=$h_par;ns=$ns;sig8=$sig8;boxsize=$boxsize;Npart=$Npart;z_in=$z_in;cosmo=$cosmology;bary=$bary" > $dir_mono/$simnm/param_$rund.info
-
+echo "Om0=$Om0;OmL=$OmL;Omb=$Omb;h_par=$h_par;ns=$ns;sig8=$sig8;boxsize=$boxsize;Npart=$Npart;z_in=$z_in;cosmo=$cosmology;softlen1=$softlen1;timestep=$tstep;bary=$bary;ngenic=$ngenic;seed=$seed" > $dir_gad/$simnm/param_$rund.info
+#To generate parameter file for monofonIC
+echo "Om0=$Om0;OmL=$OmL;Omb=$Omb;h_par=$h_par;ns=$ns;sig8=$sig8;boxsize=$boxsize;Npart=$Npart;z_in=$z_in;cosmo=$cosmology;bary=$bary;seed=$seed" > $dir_mono/$simnm/param_$rund.info
+#To generate parameter file for MUSIC
+echo "Om0=$Om0;OmL=$OmL;Omb=$Omb;h_par=$h_par;ns=$ns;sig8=$sig8;boxsize=$boxsize;Npart=$Npart;z_in=$z_in;cosmo=$cosmology;bary=$bary;seed=$seed" > $dir_music/$simnm/param_$rund.info
 # echo "simnm="L${boxsize}_N${Npart}_C${cosmology}" rund=${4:-r1}"
 
 # $dir_gad/compile.sh
--- scripts-common/bash/multi-jobs.sh
+++ scripts-common/bash/multi-jobs.sh
@@ -5,8 +5,9 @@ Npart=${2:-512}
 cosmology=${3:-p18}
 export simnm="L${boxsize}_N${Npart}_C${cosmology}" rund=${rund:-r1} seed=${seed-8899}
 export softlen1=${softlen1:-0.0065} timestep=${tstep:-0.01} z_in=${z_in:-24}
+export bary=${bary:-no} ngenic=${ngenic:-no}
 compgad=${compgad:-1}
-bary=${bary:-no}
+
 
 if [ "$bary" = yes ]; then export simnm=${simnm}_bar; fi
 
@@ -25,13 +26,20 @@ if ((compgad)); then gadget4/compile.sh; fi
 # export rund=${runds[i]} seed=${seeds[i]};
 # create-siminfo.sh $boxsize $Npart $cosmology $rund $z_in
 
-jidmono=$(qsub monofonic/comp_ics.pbs -v "simnm=$simnm,rund=$rund,seed=$seed")
+# jidics=$(qsub monofonic/comp_ics.pbs -v "simnm=$simnm,rund=$rund,seed=$seed")
 
-jidgad=$(qsub gadget4/runsim.pbs -v "simnm=$simnm,rund=$rund"  -W depend=afterok:${jidmono%.*})
+if [ "$ngenic" = "yes" ]
+then
+jidgad=$(qsub gadget4/runsim.pbs -v "simnm=$simnm,rund=$rund")
+else
+jidics=$(qsub monofonic/comp_ics.pbs -v "simnm=$simnm,rund=$rund,seed=$seed")
+jidgad=$(qsub gadget4/runsim.pbs -v "simnm=$simnm,rund=$rund"  -W depend=afterok:${jidics%.*})
+fi
 
-jidvr=$(qsub velociraptor/runstf.pbs -v "simnm=$simnm,rund=$rund,space=6d"  -W depend=afterok:${jidgad%.*})
-jidrs=$(qsub rockstar/runrstar.pbs -v "simnm=$simnm,rund=$rund"  -W depend=afterok:${jidgad%.*})
 
-jidtf=$(qsub treefrog/runtree.pbs -v "simnm=$simnm,rund=$rund,space=6d"  -W depend=afterok:${jidvr%.*});
+# jidvr=$(qsub velociraptor/runstf.pbs -v "simnm=$simnm,rund=$rund,space=6d"  -W depend=afterok:${jidgad%.*})
+# jidrs=$(qsub rockstar/runrstar.pbs -v "simnm=$simnm,rund=$rund"  -W depend=afterok:${jidgad%.*})
+
+# jidtf=$(qsub treefrog/runtree.pbs -v "simnm=$simnm,rund=$rund,space=6d"  -W depend=afterok:${jidvr%.*});
 
 # done;
