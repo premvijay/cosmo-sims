75fccc92d0d5926a0ca5103c91b304b43554914d
cprem <premv@iucaa.in>
2021-04-10 01:12:55 +0530

    reduce memlimit to avoid out of memory murders

--- gadget4/gen-param.sh
+++ gadget4/gen-param.sh
@@ -129,7 +129,7 @@ MinEgySpec 		50.0
 #PartAllocFactor       1.6
 #TreeAllocFactor       0.8
 #BufferSize            250          % in MByte
-MaxMemSize             9000
+MaxMemSize             8000
 
 % System of units
 
