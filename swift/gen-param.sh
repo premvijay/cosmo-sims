275ac9991442f4b75f4f04c329da25d8448a76fc
cprem <premv@iucaa.in>
2021-07-19 17:44:48 +0530

    units fixed

--- swift/gen-param.sh
+++ swift/gen-param.sh
@@ -11,8 +11,7 @@
 echo $2
 eval "$2"
 
-softlen0=$softlen1
-softlen2=$softlen1
+
 
 dir_root=$HOME/cosmo-sims/swift/
 dir_sim=$dir_root/$simnm/
@@ -28,6 +27,9 @@ a_in=$(python -c "print (1.0/(1+$z_in))")
 Ocdm=$(python -c "print ($Om0-$Omb)")
 hMsun10=$(python -c "print (1.98848e43/$h_par)")
 hMpc=$(python -c "print (3.08567758e24/$h_par)")
+
+softlen=$(python -c "print($softlen1*$hMpc)")
+# softlen2=$softlen1
 
 printf '%s\n' "#
 # Define some meta-data about the simulation
@@ -58,10 +60,10 @@ Gravity:
   MAC:                           adaptive  # Choice of mulitpole acceptance criterion: 'adaptive' OR 'geometric'.
   epsilon_fmm:                   0.001     # Tolerance parameter for the adaptive multipole acceptance criterion.
   theta_cr:                      0.7       # Opening angle for the purely gemoetric criterion.
-  comoving_DM_softening:         $softlen1 # Comoving Plummer-equivalent softening length for DM particles (in internal units).
-  max_physical_DM_softening:     $softlen1    # Maximal Plummer-equivalent softening length in physical coordinates for DM particles (in internal units).
-  comoving_baryon_softening:     $softlen0 # Comoving Plummer-equivalent softening length for baryon particles (in internal units).
-  max_physical_baryon_softening: $softlen0    # Maximal Plummer-equivalent softening length in physical coordinates for baryon particles (in internal units).
+  comoving_DM_softening:         $softlen # Comoving Plummer-equivalent softening length for DM particles (in internal units).
+  max_physical_DM_softening:     $softlen    # Maximal Plummer-equivalent softening length in physical coordinates for DM particles (in internal units).
+  comoving_baryon_softening:     $softlen # Comoving Plummer-equivalent softening length for baryon particles (in internal units).
+  max_physical_baryon_softening: $softlen    # Maximal Plummer-equivalent softening length in physical coordinates for baryon particles (in internal units).
   #softening_ratio_background:    0.04      # Fraction of the mean inter-particle separation to use as Plummer-equivalent softening for the background DM particles.
 
 # Parameters governing the time integration (Set dt_min and dt_max to the same value for a fixed time-step run.)
@@ -73,19 +75,19 @@ TimeIntegration:
 Snapshots:
   basename:   snapshot      # Common part of the name of output files.
   subdir:     snapdir         # (Optional) Sub-directory in which to write the snapshots. Defaults to "" (i.e. the directory where SWIFT is run).
-  scale_factor_first: 0.1 # (Optional) Scale-factor of the first snapshot if cosmological time-integration.
-  delta_time: 1.01        # Time difference between consecutive outputs (in internal units)
+  scale_factor_first: $a_in # (Optional) Scale-factor of the first snapshot if cosmological time-integration.
+  delta_time: 1.2        # Time difference between consecutive outputs (in internal units)
   invoke_stf: 0           # (Optional) Call VELOCIraptor every time a snapshot is written irrespective of the VELOCIraptor output strategy.
   invoke_fof: 0           # (Optional) Call FOF every time a snapshot is written
   compression: 0          # (Optional) Set the level of GZIP compression of the HDF5 datasets [0-9]. 0 does no compression. The lossless compression is applied to *all* the fields.
   distributed: 0          # (Optional) When running over MPI, should each rank write a partial snapshot or do we want a single file? 1 implies one file per MPI rank.
-  UnitMass_in_cgs:     1  # (Optional) Unit system for the outputs (Grams)
-  UnitLength_in_cgs:   1  # (Optional) Unit system for the outputs (Centimeters)
-  UnitVelocity_in_cgs: 1  # (Optional) Unit system for the outputs (Centimeters per second)
+  UnitMass_in_cgs:     $hMsun10   # (Optional) Unit system for the outputs (Grams)
+  UnitLength_in_cgs:   $hMpc  # (Optional) Unit system for the outputs (Centimeters)
+  UnitVelocity_in_cgs: 1e5  # (Optional) Unit system for the outputs (Centimeters per second)
   UnitCurrent_in_cgs:  1  # (Optional) Unit system for the outputs (Amperes)
   UnitTemp_in_cgs:     1  # (Optional) Unit system for the outputs (Kelvin)
   output_list_on:      $outlston  # (Optional) Enable the output list
-  output_list:         $dir_root/outputs-many-lin.list # (Optional) File containing the output times (see documentation in Parameter File section)
+  output_list:         $dir_root/outputs-few.list # (Optional) File containing the output times (see documentation in Parameter File section)
 
 # Parameters governing the conserved quantities statistics
 Statistics:
